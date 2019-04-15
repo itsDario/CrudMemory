10b728c207ad084b761e76e741db0e309afd74e0
github username <itsDario1@gmail.com>
2019-04-15 16:22:26 -0400

    added models

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,7 @@
+class UserController < ApplicationController
+  def username
+  end
+
+  def password_digest
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'user/username'
+  get 'user/password_digest'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
