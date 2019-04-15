72c7738e3a785c96a9b87bd98a9a8707c9d7baca
github username <itsDario1@gmail.com>
2019-04-15 16:34:22 -0400

    added routes models and blank index

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get 'user/username'
   get 'user/password_digest'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/', to: 'application#home'
 end
