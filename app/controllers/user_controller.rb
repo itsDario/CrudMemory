9194a34deff38687c4cc4fec949ae0f09066fd4d
github username <itsDario1@gmail.com>
2019-04-15 21:05:12 -0400

    working on making a new account

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,11 @@
-class UserController < ApplicationController
-  def username
-  end
+# frozen_string_literal: true
 
-  def password_digest
+class UserController < ApplicationController
+  def username; end
+
+  def password_digest; end
+
+  def new
+    @new = User.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'user/username'
   get 'user/password_digest'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/', to: 'application#home'
+  get '/sign_up', to: 'user#new'
+  get '/users', to: 'user#index'
 end
