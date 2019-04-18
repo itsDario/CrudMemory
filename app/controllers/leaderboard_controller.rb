897f75de824b5c00494a9cb1f1084e72384da751
github username <itsDario1@gmail.com>
2019-04-18 17:00:11 -0400

    fixed leaderboard issue with less then 10 scores

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -3,6 +3,8 @@
 class LeaderboardController < ApplicationController
   def index
     @sorted_scores = Score.all.sort { |a, b| b[:score] <=> a[:score] }
+    @max_positions = sorted_scores.length
+    @max_positions = 10 if @max_positions > 10
     # byebug
   end
 end
